15abe1598bcec1231fb557645c6d5f2ed960bbbb
TruongGiang2501 <giangtb2501@gmail.com>
2021-11-05 22:38:42 +0700

    add form Báo Cáo

---
+++ QLDSV/FrmBCDiem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLDSV
+{
+    public partial class FrmBCDiem : Form
+    {
+        public FrmBCDiem()
+        {
+            InitializeComponent();
+        }
+        Ketnoi kn = new Ketnoi();
+        private void FrmBCDiem_Load(object sender, EventArgs e)
+        {
+            DataTable dta = new DataTable();
+            dta = kn.Lay_DuLieu("select*from DIEMHOCPHAN ");
+            BCDIEMSV BC = new BCDIEMSV();    // KHỞI TẠO BÁO CÁO
+            BC.SetDataSource(dta);
+            CRV.ReportSource = BC;
+        }
+    }
+}
--- QLDSV/Program.cs
+++ QLDSV/Program.cs
@@ -16,7 +16,7 @@ namespace QLDSV
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Frm_SinhVien());
+            Application.Run(new FrmBCDiem());
         }
     }
 }
