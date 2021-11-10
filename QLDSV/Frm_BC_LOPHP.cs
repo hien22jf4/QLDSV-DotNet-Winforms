5d5c18a9bb976fc7eee0108327b7ec867b1b54c0
Lockig <minhntt2001hn@gmail.com>
2021-11-10 21:03:46 +0700

    .

--- QLDSV/Frm_BC_LOPHP.cs
+++ QLDSV/Frm_BC_LOPHP.cs
@@ -28,20 +28,25 @@ namespace QLDSV
 
         private void btn_print_Click(object sender, EventArgs e)
         {
-            SqlCommand cmd = new SqlCommand();
-            SqlDataAdapter ada = new SqlDataAdapter();
-            cmd.CommandText = "Select SINHVIEN.id_sv,SINHVIEN.ten_sv,SINHVIEN.dob_sv,LOPHOCPHAN.ten_lhp from SINHVIEN,LOPHOCPHAN,SINHVIEN_LOPHP " +
-                "where SINHVIEN.id_sv = SINHVIEN_LOPHP.id_sv and SINHVIEN_LOPHP.id_lhp = LOPHOCPHAN.id_lhp and LOPHOCPHAN.ten_lhp  = @ten_lhp";
-            cmd.Parameters.Clear();
-            cmd.Parameters.AddWithValue("@ten_lhp", cbo.SelectedValue.ToString());
-            ada.SelectCommand = cmd;
+            //SqlCommand cmd = new SqlCommand();
+            //SqlDataAdapter ada = new SqlDataAdapter();
+            //cmd.CommandText = "Select SINHVIEN.id_sv,SINHVIEN.ten_sv,SINHVIEN.dob_sv,LOPHOCPHAN.ten_lhp from SINHVIEN,LOPHOCPHAN,SINHVIEN_LOPHP " +
+            //    "where SINHVIEN.id_sv = SINHVIEN_LOPHP.id_sv and SINHVIEN_LOPHP.id_lhp = LOPHOCPHAN.id_lhp and LOPHOCPHAN.ten_lhp  = @ten_lhp";
+            //cmd.Parameters.Clear();
+            //cmd.Parameters.AddWithValue("@ten_lhp", cbo.SelectedValue.ToString());
+            //ada.SelectCommand = cmd;
             DataTable dta = new DataTable();
-            ada.Fill(dta);
+            dta = kn.Lay_DuLieu("Select SINHVIEN.id_sv,SINHVIEN.ten_sv,SINHVIEN.dob_sv,LOPHOCPHAN.ten_lhp from SINHVIEN,LOPHOCPHAN,SINHVIEN_LOPHP " +
+                "where SINHVIEN.id_sv = SINHVIEN_LOPHP.id_sv and SINHVIEN_LOPHP.id_lhp = LOPHOCPHAN.id_lhp and LOPHOCPHAN.ten_lhp  = '" + cbo.Text + "'");
             DSSV_LOPHP rpt = new DSSV_LOPHP();
             rpt.SetDataSource(dta);
-            Frm_BC_LOPHP f = new Frm_BC_LOPHP();
-            f.crystalReportViewer1.ReportSource = rpt;
-            f.ShowDialog();
+            crystalReportViewer1.ReportSource = rpt;
+            //ada.Fill(dta);
+            //DSSV_LOPHP rpt = new DSSV_LOPHP();
+            //rpt.SetDataSource(dta);
+            //Frm_BC_LOPHP f = new Frm_BC_LOPHP();
+            //f.crystalReportViewer1.ReportSource = rpt;
+            //f.ShowDialog();
         }
     }
 }
